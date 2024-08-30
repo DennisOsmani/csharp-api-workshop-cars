18d10d47caf2f647a079beaec6fb3e4648ec7004
Dennis Rizah Nordvi Osmani <dennis.osmani@no.experis.com>
2024-08-30 09:50:44 +0200

    inmemeory

--- workshop.wwwapi/Repository/CarRepository.cs
+++ workshop.wwwapi/Repository/CarRepository.cs
@@ -3,17 +3,30 @@ using workshop.wwwapi.Models;
 
 namespace workshop.wwwapi.Repository
 {
+
+
+
     public class CarRepository : IRepository
     {
+
+        DataContext _dbContext; 
+
+        public CarRepository(DataContext db)
+        {
+            this._dbContext = db;
+        }
+
         public Car AddCar(Car entity)
         {
-            CarData.AddCar(entity);
+            // _dbContext.Cars.Add(entity);
+            _dbContext.Add(entity);
+            _dbContext.SaveChanges();
             return entity;
         }
 
         public List<Car> GetCars()
         {
-            return CarData.GetCars();
+           return _dbContext.Cars.ToList();
         }
     }
 }
